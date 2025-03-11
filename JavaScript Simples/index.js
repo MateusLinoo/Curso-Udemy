ce56114befad0de208732ee7d229a6155eb78713
mNousk <mateus.malinouskas@gmail.com>
2025-03-11 14:59:44 -0300

    Primeiro commit

---
+++ JavaScript Simples/index.js
@@ -0,0 +1,13 @@
+console.log('mateus malinouskas'); //não é possivel colocar aspas simples no texto
+console.log("mateus malinouskas"); //não é possivel colocar aspas duplas no texto
+console.log(`mateus Malinouskas`); //usa para tamplate strings
+// String = Texto
+// Number = Número
+
+console.log(123456); // numeros não podem ser colocado entre aspas
+console.log(123456, 13.324);
+
+
+// exercicios //
+
+console.log('Meu nome é "Mateus". Estou aprendendo JavaScript às', 15, 'da tarde');
