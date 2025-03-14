97f815d7127094aec6b9f2cc4bd0a7353c18275a
mNousk <mateus.malinouskas@gmail.com>
2025-03-14 16:15:28 -0300

    Aula 11

---
+++ JavaScript Simples/Aula 11/index.js
@@ -0,0 +1,43 @@
+/**
+ * Aritméticos
+ * + adição / concatenação (unir dois valores)
+ * - / *
+ * ** Potenciação
+ * % Resto da divisão
+ */
+
+const num1 = 10
+const num2 = 3
+console.log(num1 - num2)
+
+// Multiplicação será sempre feita andes da adição ou da subtração
+// podemos usar os parenteses para alterar a ordem dos fatores
+
+// ORDEM DOS FATORES
+/**
+ * ()
+ * **
+ * *, /, %
+ * +, -
+ */
+
+let contador = 1
+contador++ // ++ sempre adicionará 1 // -- irá subtrair
+console.log(contador)
+// sempre incrementar antes de dar o console.log para ter uma boa pratica de programação
+
+let contador1 = 0
+contador1 += 2
+contador1 += 2
+contador1 += 2
+console.log(contador1)
+
+// NaN - Not a Number
+const num5 = 10
+const num6 = 'mateus'
+console.log(num6 * num5)
+
+// parseInt retorna numero interio
+// parseFloat retorna numero com casas decimais
+// Number identifica qual é o numero
+
