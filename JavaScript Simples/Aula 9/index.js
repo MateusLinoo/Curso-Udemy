fccc50cf521d86a40231fcbfcc87a42479c6f71f
mNousk <mateus.malinouskas@gmail.com>
2025-03-14 15:32:59 -0300

    Aula 9

---
+++ JavaScript Simples/Aula 9/index.js
@@ -0,0 +1,13 @@
+// diferença de Var e Let 1 
+
+var nome = 'Mateus'
+var nome = 'Malinouskas' 
+console.log(nome)
+// a variavel em var será redeclarada, em let será diferente
+
+// podemos também abreviar o var colocando assim:
+nome = 'Sophia' // mas não faça isso, irá gerar uma variavel global no sistema
+
+let nome = 'Mateus'
+let nome = 'Malinouskas'
+// com let não podemos declarar e redeclarar depois pois irá dar um erro
